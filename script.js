3bafff74fa83e4d803f3a3608b69290383b765a0
Angel-Gurung <Nimchuki111@gmail.com>
2024-08-27 00:02:42 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.classList.add('active');
+        } else {
+            slide.classList.remove('active');
+        }
+    });
+    
+    document.querySelector('.slides').style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Initialize first slide as active
+showSlide(currentSlide);
